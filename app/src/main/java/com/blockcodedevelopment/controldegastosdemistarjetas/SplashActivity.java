792ad156a7518684e3750c932f4f2afa2acc31e5
Andry <andry.jose.v@gmail.com>
2018-12-20 20:38:15 -0400

    ADmob agregado

--- app/src/main/java/com/blockcodedevelopment/controldegastosdemistarjetas/SplashActivity.java
+++ app/src/main/java/com/blockcodedevelopment/controldegastosdemistarjetas/SplashActivity.java
@@ -1,13 +1,31 @@
 package com.blockcodedevelopment.controldegastosdemistarjetas;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private final int DURATION_SPLASH = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, SelectCardActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURATION_SPLASH);
     }
 }
