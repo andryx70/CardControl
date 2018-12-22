e2fcd5eff16234048c0de82eba28ee7747cd4753
Andry <andry.jose.v@gmail.com>
2018-12-21 21:23:20 -0400

    layout de view card agregado e intent

---
+++ app/src/main/java/com/blockcodedevelopment/controldegastosdemistarjetas/AddCreditCardActivity.java
@@ -0,0 +1,13 @@
+package com.blockcodedevelopment.controldegastosdemistarjetas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCreditCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_credit_card);
+    }
+}
--- app/src/main/java/com/blockcodedevelopment/controldegastosdemistarjetas/SelectCardActivity.java
+++ app/src/main/java/com/blockcodedevelopment/controldegastosdemistarjetas/SelectCardActivity.java
@@ -1,5 +1,6 @@
 package com.blockcodedevelopment.controldegastosdemistarjetas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,10 +21,14 @@ public class SelectCardActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                intentAddCard();
             }
         });
+    }
+
+    public void intentAddCard(){
+        Intent intent = new Intent(this, AddCreditCardActivity.class);
+        startActivity(intent);
     }
 
 }
